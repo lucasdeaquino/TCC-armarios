011987fa1fe38e59201228dd0a1506aa5ba06faf
bill-coder <51916953+bill-coder@users.noreply.github.com>
2019-08-05 10:38:38 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -2,11 +2,14 @@
 <html>
 <head>
 	<title>LOGIN</title>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
+
 
 <body>
    <?php 
@@ -16,9 +19,10 @@
       <nav>
        <div id="legal"> <img src="imagens/legal1.png" width="80%" ><br><br></div>
   <form method="post">
-	 <input type="text" name="rm" id="rm" placeholder="coloque seu rm">
-	   <input type="password" name="senha" id="senha" placeholder="digite a sua senha">
+	<div id="l"> <input type="text" name="rm" id="rm" placeholder="Coloque seu rm"><br><br>
+	   <input type="password" name="senha" id="senha" placeholder="Digite a sua senha"></div><br><br>
 	 <input type="button" name="enviar" id="enviar" value="enviar" class="botao1">
+  <div id="top"><a href="cadastro.php" id="le">Cadastre-se</a></div>
    
 	  <script type="text/javascript">
 	
@@ -54,6 +58,13 @@
 </html>
 
 <style type="text/css">
+
+#le{
+
+color:white;
+
+}
+
 body{
   background-image:url("imagens/issoai.jpg");
   background-size:cover;
@@ -66,6 +77,8 @@ nav{
   width:40%; 
   height:400px;
   padding-left: 5%;
+  font-family: 'Montserrat', sans-serif;
+
 }
 
 img{
@@ -76,13 +89,26 @@ img{
 
 .botao1{
 
-color: #2914A4;
-padding: 15px 32px;
+color: black;
+padding: 5px 10px;
 text-align: center;
-text-decoration: none;
+font-family: 'Montserrat', sans-serif;
 display: inline-block;
 font-size: 16px;
+border-radius: 30%;
+margin-left: 31%;
 
+}
+
+#l{
+ 
+margin-left: 17%;
+
+}
+
+#top{
+margin-left: 28%;
+margin-top: -16%;
 }
 
 </style>
