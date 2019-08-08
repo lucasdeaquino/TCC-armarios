8e3773c95419d6bf69e85e8d5888dedebc545fc3
lucasdeaquino <luquinhasaquino91@gmail.com>
2019-08-08 08:39:03 -0300

    dia 08/08

--- Ajax/cadastro-aluno-ajax.php
+++ Ajax/cadastro-aluno-ajax.php
@@ -9,14 +9,22 @@ $turma = $_POST['turma'];
 $email = $_POST['email'];
 $telefone  = $_POST['telefone'];
 
- $insertaluno = "INSERT into tb_aluno values (null, '$nome', 'a.png', '$rm', '$email', null, '$turma', '$senha')";
+ $insertaluno = "INSERT into tb_aluno values ('$rm', '$nome', 'a.png',  '$email', '$senha')";
   if(!$exeinsert = $mysqli -> query($insertaluno)){
   	echo $mysqli ->error;
   }
  
   else{
 
+  	$insert_turma = "INSERT into tb_turma_aluno values (null, '$turma', '$rm')";
+
+     if(!$exe_turma = $mysqli -> query($insert_turma)){
+  	echo $mysqli ->error;
+  }
+
+    else{
   	echo 'Cadastro realizado com sucesso.';
   }
+}
 
 ?>
--- Ajax/login-ajax.php
+++ Ajax/login-ajax.php
@@ -8,7 +8,7 @@ include('../conexao.php');
   $senha = $_POST['senha'];
   
 
-  $consultuser = "SELECT * from tb_aluno where ds_rm = '$rm' and ds_senha = '$senha'";
+  $consultuser = "SELECT * from tb_aluno where cd_rm = '$rm' and ds_senha = '$senha'";
   if(!$exeuser = $mysqli -> query($consultuser)){
 
   	echo $mysqli ->error;
@@ -27,7 +27,9 @@ include('../conexao.php');
        }
 
        else{
-           
+       
+       // Consultando se existe um administrador
+
             $consultadm = "SELECT * from tb_adm where nm_adm = '$rm' and senha_adm = '$senha'";
 
             if(!$exeadm = $mysqli -> query($consultadm)){
@@ -40,10 +42,10 @@ include('../conexao.php');
 
         if($exeadm -> num_rows > 0){
  
-        // Se existir, ir para a página inicial
+        // Se existir, ir para a página inicial de administrador
   
 
-          echo "location.href = 'inicial.php';";
+          echo "location.href = 'administrador/adm-inicial.php';";
 
        }
 
