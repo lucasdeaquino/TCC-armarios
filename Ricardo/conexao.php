3fa1f341f8fb958901c1e53939800aed53eaf043
Ricardin012 <51785783+Ricardin012@users.noreply.github.com>
2019-08-12 17:04:54 -0300

    Add files via upload

--- Ricardo/conexao.php
+++ Ricardo/conexao.php
@@ -3,11 +3,12 @@ $endereco = "localhost";
 $login = "root";
 $senha = "usbw";
 $banco = "db_tcc";
+echo "<img src='legal1.jpg' width='80%' >";
 $mysqli = new mysqli($endereco , $login, $senha, $banco);
 if(mysqli_connect_errno())
 {
 	echo $mysqli -> mysql_error();
-	
+
 	}
-	
+
 ?>
--- Ricardo/login.php
+++ Ricardo/login.php
@@ -5,38 +5,38 @@
   <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><img src="https://static.hentaicdn.com/hentai/19621/1b/ccdn0001.jpg" width="80%" >
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
 
 <body>
-   <?php 
+   <?php
      session_start();
      include('conexao.php');
       ?>
       <nav>
-       <div id="legal"> <img src="imagens/legal1.png" width="80%" ><br><br></div>
+       <div id="legal"> <img src="legal1.jpeg" width="80%" ><br><br></div>
   <form method="post">
 	<div id="l"> <input type="text" name="rm" id="rm" placeholder="Coloque seu rm"><br><br>
 	   <input type="password" name="senha" id="senha" placeholder="Digite a sua senha"></div><br><br>
 	 <input type="button" name="enviar" id="enviar" value="enviar" class="botao1">
   <div id="top"><a href="cadastro.php" id="le">Cadastre-se</a></div>
-   
+
 	  <script type="text/javascript">
-	
+
         	$(document).ready(function(){
   $("#enviar").click(function(){
-    
+
      var dados = {'rm': $('#rm').val(), 'senha': $('#senha').val()};
      $.ajax({
-      
+
       type: 'POST',
       url: 'Ajax/login-ajax.php',
-      data: dados, 
+      data: dados,
       success : function(response){
-             
+
              if(response.trim() == "location.href = 'inicial.php';" ){
              eval(response);
           }
@@ -50,10 +50,10 @@
   });
 });
 	   </script>
-	
+
   </form>
 </nav>
-    
+
 </body>
 </html>
 
@@ -74,7 +74,7 @@ body{
 nav{
   margin-top:12%;
   margin-left:32%;
-  width:40%; 
+  width:40%;
   height:400px;
   padding-left: 5%;
   font-family: 'Montserrat', sans-serif;
@@ -101,7 +101,7 @@ margin-left: 31%;
 }
 
 #l{
- 
+
 margin-left: 17%;
 
 }
