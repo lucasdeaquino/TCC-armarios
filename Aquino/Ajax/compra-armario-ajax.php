267883fad3d9ebdd9809fbc8b68609dce86b2850
lucasdeaquino <51785703+lucasdeaquino@users.noreply.github.com>
2019-08-26 12:16:52 -0300

    Add files via upload
    Dia 26/08

--- Aquino/Ajax/compra-armario-ajax.php
+++ Aquino/Ajax/compra-armario-ajax.php
@@ -5,17 +5,31 @@ include('../conexao.php');
 
 $id = $_POST['id'];
 
-  $info_aluno = "SELECT tb_aluno.nm_aluno as 'nome', tb_aluno.cd_rm as 'rm', tb_turmas.nm_turmas as 'turma', tb_turmas.vl_reserva_g as 'valorg', tb_turmas.vl_reserva_p as 'valorp' from tb_aluno
+//Consultando aluno
+
+  $info_aluno = "SELECT tb_aluno.nm_aluno as 'nome', tb_aluno.cd_rm as 'rm', tb_turmas.nm_turmas as 'turma' from tb_aluno
       inner join tb_turma_aluno on (tb_aluno.cd_rm = tb_turma_aluno.id_rm)
-      inner join tb_turmas on (tb_turmas.cd_turmas = tb_turma_aluno.id_turmas WHERE cd_rm = '$aluno'
-      )";
+      inner join tb_turmas on (tb_turmas.cd_turmas = tb_turma_aluno.id_turmas) WHERE cd_rm = '$aluno'
+      ";
 
   $exe_info_aluno = $mysqli -> query($info_aluno);
 
+$row = $exe_info_aluno -> fetch_object();
+
+echo "Nome: ".$row->nome." RM: ".$row->rm." Turma: ".$row->turma;
 
 
+//Consultando armário escolhido
 
+$info_armario = "SELECT  tb_armario.nm_armario as 'armario', tb_tipo_armario.ds_tipo as 'tipo', tb_tipo_armario.vl_armario as'valor' from tb_armario
+     inner join tb_tipo_armario on (tb_tipo_armario.cd_tipo_armario = tb_armario.id_tipo_armario)
+     WHERE tb_armario.cd_armario = '$id' "
+     ;
 
+  $exe_info_armario = $mysqli-> query($info_armario);
 
+  $row_armario = $exe_info_armario -> fetch_object();
+
+  echo " Armario: ".$row_armario->armario." Tipo: ".$row_armario->tipo." Valor: ".$row_armario->valor;
  
 ?>
