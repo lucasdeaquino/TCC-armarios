02bdf8dc4a59340a316179e5ead5648fe1bd9c7b
Ricardin012 <51785783+Ricardin012@users.noreply.github.com>
2019-09-17 09:33:25 -0300

    Add files via upload

--- TCC/administrador/adm-aluno.php
+++ TCC/administrador/adm-aluno.php
@@ -6,11 +6,47 @@
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <meta charset="utf-8">
  </head>
     <body>
+
+      <nav id="nav">
+    <div class="nav-wrapper black">
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="left hide-on-med-and-down">
+        <li><a id="menu" href="adm-aluno.php">Lista de Alunos</a></li>
+        <li><a id="menu" href="armarios.php">Armários</a></li>
+        <li><a id="menu" href="adm-reservas.php">Reservas</a></li>
+           <li><a id="menu" href="cadastro-adm.php">Cadastrar Administrador</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="sass.html">Sass</a></li>
+    <li><a href="badges.html">Components</a></li>
+    <li><a href="collapsible.html">Javascript</a></li>
+    <li><a href="mobile.html">Mobile</a></li>
+  </ul>
+     
+     <center> <img id="daora" src="legal1.png"></center> 
+ </div>
+
       <div class="container">
-         
+
+        <table>
+         <thead>
+            <tr>
+              <th>Nome</th>
+              <th>RM</th>
+              <th>Email</th>
+              <th>Senha</th>
+              <th>Turma</th>
+              <th>Curso</th>
+          </tr>
+        </thead>
+        
          <form action="altera-aluno.php" method="post">
           <?php
            
@@ -37,6 +73,44 @@
                   }
 
           ?>
+    </table>
     </div>
    </body>
+
+<div class="footer">
+  <p>Footer</p>
+</div>
+<meta name="viewport" content="width=device-width, initial-scale=1">
  </html>
+ <style type="text/css">
+   
+
+#menu{
+
+transition: 0.5s;
+
+}
+
+#menu:hover{
+
+ color:#6495ED;
+
+}
+
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: black;
+   color: white;
+   text-align: center;
+}
+
+#daora{
+margin-top: -5%
+   
+}
+
+
+ </style>
