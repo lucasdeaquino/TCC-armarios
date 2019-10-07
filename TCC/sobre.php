8b4dde697033b4c26c6bccf5762003bc06c72529
bill-coder <51916953+bill-coder@users.noreply.github.com>
2019-10-07 10:36:43 -0300

    Add files via upload

--- TCC/sobre.php
+++ TCC/sobre.php
@@ -1,14 +1,138 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+
 <?php
 	include('menu.php')
  ?>
 <head>
-	<title></title>
+
+
+	<title>Sobre</title>
 </head>
 <body>
 
 
 
+ <center>
+ <div id="balao1">
+
+            <div class="row">
+            <div class="col s12 m7">
+            <div class="card">
+            <div class="card-image">  
+            </div>
+            <span class="card-title">Sobre nós</span>
+            <div class="card-content">
+            <p>Este é o Etec Locker, o sistema de alugel e gerenciamento de armarios da Etec de Itanhaém, aqui voce pode adiquirir seu armario de forma rapida, facil e prática. Este porojeto foi desenvolvido por alunos da Etec de Itanhaém, mas precisasmente é um projeto de TCC feito por alunos do 3º ano de informatica, que visa informatizar o atual sistema de gerenciamento de armarios que é feito por meio do EXCEL de forma manual, assim facilitando a perca de dados e dificultando o porscesso de aluguel dos armários </p>
+            </div>
+            </div>
+            </div> 
+            </div>
+  
+</div>
+</center>
+
+
+  <div id="balao" class="container">
+  	<div class="row">
+  		<div class="col s4 m4">
+  			<div class="card">
+  				<div class="card-image">
+  					<img src="imagens/terra.jpg">
+  				</div>
+  				<div class="card-content">
+  					<p>Luiz Felipe Dos Santos</p>
+  				</div>
+
+   			</div>
+   		</div>
+   		<div class="col s4 m4">
+  			<div class="card">
+  				<div class="card-image">
+  					<img src="imagens/terra.jpg">
+  				</div>
+  				<div class="card-content">
+  					<p>Vitor Almeida </p>
+   				</div>
+   			</div>
+   		</div>
+   		<div class="col s4 m4">
+  			<div class="card">
+  				<div class="card-image">
+  					<img src="imagens/terra.jpg">
+  				</div>
+  				<div class="card-content">
+  					<p>Lucas de Aquino</p>
+   				</div>
+   			</div>
+   		</div>
+   </div>
+   <div class="row">
+   		<div class="col s4 m4">
+  			<div class="card">
+  				<div class="card-image">
+  					<img src="imagens/terra.jpg">
+  				</div>
+  				<div class="card-content">
+  					<p>João Pedro Nardes</p>
+   				</div>
+   			</div>
+   		</div>
+   		<div class="col s4 m4">
+  			<div class="card">
+  				<div class="card-image">
+  					<img src="imagens/terra.jpg">
+  				</div>
+  				<div class="card-content">
+  					<p>Pedro Ricardo</p>
+   				</div>
+   			</div>
+   		</div>
+   		<div class="col s4 m4">
+  			<div class="card">
+  				<div class="card-image">
+  					<img src="imagens/terra.jpg">
+  				</div>
+  				<div class="card-content">
+  					<p>João Vitor acaccio</p>
+   				</div>
+   			</div>
+   		</div>
+   </div>
+ </div>
+
+
+
 </body>
+
+<style type="text/css">
+	
+#balao{
+
+width: 60%;
+transition: 1s;
+}
+
+#balao:hover{
+
+width: 70%;
+
+}
+
+#balao1{
+transition: 1s;
+margin-left: 30%;
+margin-top: 2%;
+}
+
+
+
+</style>
+
+
 <?php 
 	include('footer.php');
 ?>
+
+</html>
