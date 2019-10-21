40b5c4a4bf669a50aab6336e6787d8a052d66d5b
bill-coder <51916953+bill-coder@users.noreply.github.com>
2019-10-21 11:13:45 -0300

    Add files via upload

---
+++ Luiz Felipe/menu-adm.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link rel="stylesheet" href="estiloddw6.css" type="text/css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<nav id="nav">
+    <div class="nav-wrapper indigo darken-4">
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="left hide-on-med-and-down">
+        <li><a id="menu" href="administrador/adm-inicial.php">Menu</a></li>
+        <li><a id="menu" href="administrador/adm-aluno.php">Lista de Alunos</a></li>
+        <li><a id="menu" href="administrador/armarios.php">Armários</a></li>
+        <li><a id="menu" href="administrador/adm-reservas.php">Reservas</a></li>
+        <li><a id="menu" href="administrador/cadastro-adm.php">Cadastrar Administrador</a></li>
+      </ul>
+    </div>
+    </nav>
+
+<style type="text/css">
+#menu{
+transition: 0.5s;
+}
+#menu:hover{
+ color:#6495ED;
+}
+</style>
