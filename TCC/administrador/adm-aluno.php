5ae57d4d419505b17095b61513f295917bbc3ab7
bill-coder <51916953+bill-coder@users.noreply.github.com>
2019-10-29 09:26:20 -0300

    Add files via upload

--- TCC/administrador/adm-aluno.php
+++ TCC/administrador/adm-aluno.php
@@ -88,29 +88,12 @@ AND tb_reserva.id_aluno2_reserva = tb_aluno.cd_rm )";
           
     </div>
    </body>
-   <footer class="page-footer indigo darken-4">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Footer </h5>
-                <p class="grey-text text-lighten-4">dai a gente ve o que vai escrever aki.</p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">sei la</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">alguma coisa</a></li>
 
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">outra coisa</a>
-            </div>
-          </div>
-        </footer>
+   <?php
+    include('../footer.php')
+?>
 
+ 
  </html>
  <style type="text/css">
 #menu{
