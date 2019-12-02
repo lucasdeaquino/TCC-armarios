e1f9c499801b3e4f1339193feb33bac96af2eb60
Ricardin012 <51785783+Ricardin012@users.noreply.github.com>
2019-12-02 09:33:02 -0300

    Add files via upload

---
+++ João Pedro (Golias)/menu.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <meta http-equiv= "Content-Type" content= "text/html; charset=iso-8859-1" >
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="estiloddw6.css" type="text/css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<nav id="nav">
+    <div class="nav-wrapper indigo darken-4">
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="left hide-on-med-and-down">
+        <li><a id="menu" href="home.php " style="text-decoration:none">Menu</a></li>
+        <li><a id="menu" href="aluno-armario.php " style="text-decoration:none">Comprar Armarios</a></li>
+        <li><a id="menu" href="sobre.php " style="text-decoration:none">Sobre</a></li>
+        <li><a id="menu" href="faq.php " style="text-decoration:none">Enviar Duvidas</a></li>
+        <li><a id="menu" href="ver-armario.php " style="text-decoration:none">Seu Armario</a></li>
+        <li><a id="menu" href="perfil-aluno.php " style="text-decoration:none"> Seu Perfil</a></li>
+      </ul>
+    </div>
+    </nav>
+
+
+<center><img src="imagens/legal1.png" id="daora"></center>
+
+
+<style type="text/css">
+
+#nav{
+
+  position: fixed;
+  z-index: 999;
+}
+
+#menu{
+
+transition: 0.5s;
+}
+#menu:hover{
+ color:#6495ED;
+}
+
+#etec{
+  margin-left: 740px;
+  margin-top: -3%;
+  width: 200px;
+}
+
+
+#daora{
+margin-top:1%; 
+width: 30%;
+transition: 1s;
+}
+
+#daora:hover
+{
+        -webkit-transform: scale(1.3);
+        -ms-transform: scale(1.3);
+        transform: scale(1.3);
+}
+
+</style>
